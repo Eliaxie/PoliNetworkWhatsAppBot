d6aa01100ec7b3af252fcf956c4f8a1e0482adb0
Diego Aldarese <10710677@polimi.it>
2021-11-26 20:11:15 +0100

    .

--- app.js
+++ app.js
@@ -20,43 +20,42 @@ var bannedUsers = [];
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         conn.autoReconnect = wa.ReconnectMode.onConnectionLost; // only automatically reconnect when the connection breaks
-        // conn.logger.level = 'debug' // set to 'debug' to see what kind of stuff you can implement
+        // conn.logger.level = "debug" // set to "debug" to see what kind of stuff you can implement
         // attempt to reconnect at most 10 times in a row
         conn.connectOptions.maxRetries = 10;
         // loads the auth file credentials if present
         /*  Note: one can take this auth_info.json file and login again from any computer without having to scan the QR code,
-            and get full access to one's WhatsApp. Despite the convenience, be careful with this file */
-        fs.existsSync('./auth_info.json') && conn.loadAuthInfo('./auth_info.json');
+            and get full access to one"s WhatsApp. Despite the convenience, be careful with this file */
+        fs.existsSync("./auth_info.json") && conn.loadAuthInfo("./auth_info.json");
         yield conn.connect();
         // credentials are updated on every connect
         const authInfo = conn.base64EncodedAuthInfo(); // get all the auth info we need to restore this session
         try {
-            fs.writeFileSync('./auth_info.json', JSON.stringify(authInfo, null, '\t'));
+            fs.writeFileSync("./auth_info.json", JSON.stringify(authInfo, null, "\t"));
         }
         catch (_a) {
             console.log("Unable to save login information, the QR will be asked again next time");
         } // save this info to a file
         var ready = 0;
-        conn.on('contacts-received', () => {
+        conn.on("contacts-received", () => {
             ready++;
             initialize(ready);
         });
-        conn.on('chats-received', () => {
+        conn.on("chats-received", () => {
             ready++;
             initialize(ready);
         });
-        conn.on('chat-new', chat => {
+        conn.on("chat-new", chat => {
             getGroups(chat);
         });
-        conn.on('group-update', groupMetadata => {
-            console.log(groupMetadata.subject);
+        conn.on("group-update", groupMetadata => {
             getGroups(conn.chats.get(groupMetadata.jid));
         });
         /**
          * The universal event for anything that happens
          * New messages, updated messages, read & delivered messages, participants typing etc.
          */
-        conn.on('chat-update', (chat) => __awaiter(this, void 0, void 0, function* () {
+        conn.on("chat-update", (chat) => __awaiter(this, void 0, void 0, function* () {
             // only do something when a new message is received
             if (!chat.hasNewMessage) {
                 return;
@@ -71,14 +70,13 @@ function main() {
             const messageType = Object.keys(messageContent)[0]; // message will always contain one key signifying what kind of message
             if (messageType == wa.MessageType.text && thrustedUsers.includes(sender)) {
                 const text = m.message.conversation;
-                yield conn.chatRead(m.key.remoteJid); // mark chat read
+                yield conn.chatRead(sender); // mark chat read
                 const options = { quoted: m };
                 var type;
                 type = wa.MessageType.text;
                 if (text.includes("!banAll")) {
                     var number = text.split(" ")[1];
-                    var success = yield addUser(number, "banned");
-                    if (success && (yield banUsers()) == true) {
+                    if ((yield addUser(number, "banned")) && (yield banUsers())) {
                         setTimeout(() => __awaiter(this, void 0, void 0, function* () {
                             var content = "User banned successfully!";
                             yield conn.sendMessage(sender, content, type, options);
@@ -93,7 +91,7 @@ function main() {
                 }
                 else if (text.includes("!addThrustedUser")) {
                     var number = text.split(" ")[1];
-                    if ((yield addUser(number, "thrusted")) == true) {
+                    if (yield addUser(number, "thrusted")) {
                         setTimeout(() => __awaiter(this, void 0, void 0, function* () {
                             var content = "Thrusted user added successfully!";
                             yield conn.sendMessage(sender, content, type, options);
@@ -106,11 +104,40 @@ function main() {
                         }), getRandomInt(5000));
                     }
                 }
-                else if (text.includes("!resetLinks")) {
+                else if (text == "!resetLinks") {
+                    var list = yield resetLinks();
+                    if (!list.startsWith("I couldn't save")) {
+                        setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                            var content = "Links revoked succeffully (probably, if not I just put the old one in the list)! Here are all the groups with their invitation link:";
+                            yield conn.sendMessage(sender, content, type, options);
+                            yield conn.sendMessage(sender, { url: "./groups.txt" }, wa.MessageType.document, {});
+                        }), getRandomInt(5000));
+                    }
+                    else {
+                        setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                            yield conn.sendMessage(sender, list, type, options);
+                        }), getRandomInt(5000));
+                    }
+                }
+                else if (text == "!printGroups") {
+                    var list = yield printGroups();
+                    if (!list.startsWith("I couldn't save")) {
+                        setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                            var content = "I successfully made a file with all the groups and their invitation links, here it is:";
+                            yield conn.sendMessage(sender, { url: "./groups.pdf" }, // send directly from local file
+                            wa.MessageType.document, { mimetype: wa.Mimetype.pdf, caption: content });
+                            //await conn.sendMessage(sender, { url: "./groups.txt" }, wa.MessageType.document, { caption: content })
+                        }), getRandomInt(5000));
+                    }
+                    else {
+                        setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                            yield conn.sendMessage(sender, list, type, options);
+                        }), getRandomInt(5000));
+                    }
                 }
             }
         }));
-        conn.on('close', ({ reason, isReconnecting }) => (console.log('Oh no got disconnected: ' + reason + ', reconnecting: ' + isReconnecting)));
+        conn.on("close", ({ reason, isReconnecting }) => (console.log("Oh no got disconnected: " + reason + ", reconnecting: " + isReconnecting)));
     });
 }
 function initialize(ready) {
@@ -122,14 +149,14 @@ function initialize(ready) {
 }
 function getThrustedUsers() {
     return __awaiter(this, void 0, void 0, function* () {
-        if (fs.existsSync('./thrusted_users.json')) {
+        if (fs.existsSync("./thrusted_users.json")) {
             try {
-                thrustedUsers = JSON.parse(fs.readFileSync('./thrusted_users.json').toString());
+                thrustedUsers = JSON.parse(fs.readFileSync("./thrusted_users.json").toString());
             }
             catch (_a) {
                 console.log("Unable to parse previously saved thrusted users, deleting the file and starting over");
                 try {
-                    fs.rmSync('./thrusted_users.json');
+                    fs.rmSync("./thrusted_users.json");
                     getThrustedUsers();
                 }
                 catch (_b) {
@@ -144,12 +171,12 @@ function getThrustedUsers() {
                 output: process.stdout
             });
             yield rl.question("There are no thrusted users that can give commands to this bot, please provide one via their phone number (you will be able to add more using !newThrustedUser <number> inside the bot) [example: +39 123 456 789 becomes 39123456789]\n", (answer) => __awaiter(this, void 0, void 0, function* () {
-                const exists = yield conn.isOnWhatsApp(answer);
+                var exists = yield conn.isOnWhatsApp(answer);
                 if (exists) {
                     console.log("Thrusted user added successfully, running the bot");
                     thrustedUsers.push(exists.jid);
                     try {
-                        fs.writeFileSync('./thrusted_users.json', JSON.stringify(thrustedUsers, null, '\t'));
+                        fs.writeFileSync("./thrusted_users.json", JSON.stringify(thrustedUsers, null, "\t"));
                     }
                     catch (_c) {
                         console.log("Unable to save the thrusted user to a file, this will not be persistent");
@@ -165,9 +192,9 @@ function getThrustedUsers() {
     });
 }
 function getBannedUsers() {
-    if (fs.existsSync('./banned_users.json')) {
+    if (fs.existsSync("./banned_users.json")) {
         try {
-            bannedUsers = JSON.parse(fs.readFileSync('./banned_users.json').toString());
+            bannedUsers = JSON.parse(fs.readFileSync("./banned_users.json").toString());
         }
         catch (_a) {
             console.log("Unable to parse previously saved banned users, please back them up and delete the file");
@@ -220,10 +247,10 @@ function addUser(number, type) {
             }
             try {
                 if (type == "thrusted") {
-                    fs.writeFileSync('./thrusted_users.json', JSON.stringify(thrustedUsers, null, '\t'));
+                    fs.writeFileSync("./thrusted_users.json", JSON.stringify(thrustedUsers, null, "\t"));
                 }
                 else {
-                    fs.writeFileSync('./banned_users.json', JSON.stringify(bannedUsers, null, '\t'));
+                    fs.writeFileSync("./banned_users.json", JSON.stringify(bannedUsers, null, "\t"));
                 }
             }
             catch (_a) {
@@ -242,18 +269,34 @@ function banUsers() {
     });
 }
 function resetLinks() {
-    var successfull = [];
-    var failed = [];
-    myGroups.forEach((element) => __awaiter(this, void 0, void 0, function* () {
-        try {
-            var newLink = yield conn.revokeInvite(element);
-            successfull.push(newLink);
-            successfull.push(element);
+    return __awaiter(this, void 0, void 0, function* () {
+        var myGroupsLinksTmp = [];
+        for (var i = 0; i < myGroups.length; i++) {
+            setTimeout(() => __awaiter(this, void 0, void 0, function* () {
+                try {
+                    myGroupsLinksTmp.push(yield conn.revokeInvite(myGroups[i].jid));
+                }
+                catch (_a) {
+                    myGroupsLinksTmp.push(myGroupsLinks[i]);
+                }
+            }), getRandomInt(1000));
         }
-        catch (_a) {
-            failed.push(element);
-        }
-    }));
+        myGroupsLinks = myGroupsLinksTmp;
+        return printGroups();
+    });
+}
+function printGroups() {
+    var list = "";
+    for (var i = 0; i < myGroups.length; i++) {
+        list += myGroups[i].name + "\nhttps://chat.whatsapp.com/" + myGroupsLinks[i] + "\n\n";
+    }
+    try {
+        fs.writeFileSync("./groups.txt", list);
+        return list;
+    }
+    catch (_a) {
+        return "I couldn't save what I did to a file, i'll try to append it here\n" + list;
+    }
 }
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import * as wa from '@adiwajshing/baileys'
-import * as fs from 'fs'
-import * as readline from 'readline'
+import * as wa from "@adiwajshing/baileys"
+import * as fs from "fs"
+import * as readline from "readline"
 
 const conn = new wa.WAConnection() // instantiate
 var myGroups = []
@@ -10,38 +10,38 @@ var bannedUsers = []
 
 async function main() {
     conn.autoReconnect = wa.ReconnectMode.onConnectionLost // only automatically reconnect when the connection breaks
-    // conn.logger.level = 'debug' // set to 'debug' to see what kind of stuff you can implement
+    // conn.logger.level = "debug" // set to "debug" to see what kind of stuff you can implement
     // attempt to reconnect at most 10 times in a row
     conn.connectOptions.maxRetries = 10
 
     // loads the auth file credentials if present
     /*  Note: one can take this auth_info.json file and login again from any computer without having to scan the QR code, 
-        and get full access to one's WhatsApp. Despite the convenience, be careful with this file */
-    fs.existsSync('./auth_info.json') && conn.loadAuthInfo('./auth_info.json')
+        and get full access to one"s WhatsApp. Despite the convenience, be careful with this file */
+    fs.existsSync("./auth_info.json") && conn.loadAuthInfo("./auth_info.json")
 
     await conn.connect()
     // credentials are updated on every connect
     const authInfo = conn.base64EncodedAuthInfo() // get all the auth info we need to restore this session
     try {
-        fs.writeFileSync('./auth_info.json', JSON.stringify(authInfo, null, '\t'))
+        fs.writeFileSync("./auth_info.json", JSON.stringify(authInfo, null, "\t"))
     } catch {
         console.log("Unable to save login information, the QR will be asked again next time")
     } // save this info to a file
 
     var ready = 0
-    conn.on('contacts-received', () => {
+    conn.on("contacts-received", () => {
         ready++
         initialize(ready)
     })
-    conn.on('chats-received', () => {
+    conn.on("chats-received", () => {
         ready++
         initialize(ready)
     })
 
-    conn.on('chat-new', chat => {
+    conn.on("chat-new", chat => {
         getGroups(chat)
     })
-    conn.on('group-update', groupMetadata => {
+    conn.on("group-update", groupMetadata => {
         getGroups(conn.chats.get(groupMetadata.jid))
     })
 
@@ -49,7 +49,7 @@ async function main() {
      * The universal event for anything that happens
      * New messages, updated messages, read & delivered messages, participants typing etc.
      */
-    conn.on('chat-update', async (chat) => {
+    conn.on("chat-update", async (chat) => {
         // only do something when a new message is received
         if (!chat.hasNewMessage) {
             return
@@ -67,14 +67,13 @@ async function main() {
         const messageType = Object.keys(messageContent)[0] // message will always contain one key signifying what kind of message
         if (messageType == wa.MessageType.text && thrustedUsers.includes(sender)) {
             const text = m.message.conversation
-            await conn.chatRead(m.key.remoteJid) // mark chat read
+            await conn.chatRead(sender) // mark chat read
             const options: wa.MessageOptions = { quoted: m }
             var type: wa.MessageType
             type = wa.MessageType.text
             if (text.includes("!banAll")) {
                 var number = text.split(" ")[1]
-                var success = await addUser(number, "banned")
-                if (success && (await banUsers()) == true) {
+                if ((await addUser(number, "banned")) && (await banUsers())) {
                     setTimeout(async () => {
                         var content = "User banned successfully!"
                         await conn.sendMessage(sender, content, type, options)
@@ -87,7 +86,7 @@ async function main() {
                 }
             } else if (text.includes("!addThrustedUser")) {
                 var number = text.split(" ")[1]
-                if ((await addUser(number, "thrusted")) == true) {
+                if (await addUser(number, "thrusted")) {
                     setTimeout(async () => {
                         var content = "Thrusted user added successfully!"
                         await conn.sendMessage(sender, content, type, options)
@@ -98,14 +97,43 @@ async function main() {
                         await conn.sendMessage(sender, content, type, options)
                     }, getRandomInt(5000))
                 }
-            } else if (text.includes("!resetLinks")) {
-                
+            } else if (text == "!resetLinks") {
+                var list = await resetLinks()
+                if (!list.startsWith("I couldn't save")) {
+                    setTimeout(async () => {
+                        var content = "Links revoked succeffully (probably, if not I just put the old one in the list)! Here are all the groups with their invitation link:"
+                        await conn.sendMessage(sender, content, type, options)
+                        await conn.sendMessage(sender, { url: "./groups.txt" }, wa.MessageType.document, { })
+                    }, getRandomInt(5000))
+                } else {
+                    setTimeout(async () => {
+                        await conn.sendMessage(sender, list, type, options)
+                    }, getRandomInt(5000))
+                }
+            } else if (text == "!printGroups") {
+                var list = await printGroups()
+                if (!list.startsWith("I couldn't save")) {
+                    setTimeout(async () => {
+                        var content = "I successfully made a file with all the groups and their invitation links, here it is:"
+                        await conn.sendMessage(
+                            sender,
+                            { url: "./groups.pdf" }, // send directly from local file
+                            wa.MessageType.document,
+                            { mimetype: wa.Mimetype.pdf, caption: content }
+                        )
+                        //await conn.sendMessage(sender, { url: "./groups.txt" }, wa.MessageType.document, { caption: content })
+                    }, getRandomInt(5000))
+                } else {
+                    setTimeout(async () => {
+                        await conn.sendMessage(sender, list, type, options)
+                    }, getRandomInt(5000))
+                }
             }
         }
     })
 
-    conn.on('close', ({ reason, isReconnecting }) => (
-        console.log('Oh no got disconnected: ' + reason + ', reconnecting: ' + isReconnecting)
+    conn.on("close", ({ reason, isReconnecting }) => (
+        console.log("Oh no got disconnected: " + reason + ", reconnecting: " + isReconnecting)
     ))
 }
 
@@ -118,13 +146,13 @@ function initialize(ready) {
 }
 
 async function getThrustedUsers() {
-    if (fs.existsSync('./thrusted_users.json')) {
+    if (fs.existsSync("./thrusted_users.json")) {
         try {
-            thrustedUsers = JSON.parse(fs.readFileSync('./thrusted_users.json').toString())
+            thrustedUsers = JSON.parse(fs.readFileSync("./thrusted_users.json").toString())
         } catch {
             console.log("Unable to parse previously saved thrusted users, deleting the file and starting over")
             try {
-                fs.rmSync('./thrusted_users.json')
+                fs.rmSync("./thrusted_users.json")
                 getThrustedUsers()
             } catch {
                 console.log("Unable to delete the old file, please fix the problem yourself")
@@ -135,17 +163,17 @@ async function getThrustedUsers() {
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
-        });
+        })
 
         await rl.question("There are no thrusted users that can give commands to this bot, please provide one via their phone number (you will be able to add more using !newThrustedUser <number> inside the bot) [example: +39 123 456 789 becomes 39123456789]\n", async (answer) => {
-            const exists = await conn.isOnWhatsApp(answer)
+            var exists = await conn.isOnWhatsApp(answer)
 
             if (exists) {
                 console.log("Thrusted user added successfully, running the bot")
                 thrustedUsers.push(exists.jid)
 
                 try {
-                    fs.writeFileSync('./thrusted_users.json', JSON.stringify(thrustedUsers, null, '\t'))
+                    fs.writeFileSync("./thrusted_users.json", JSON.stringify(thrustedUsers, null, "\t"))
                 } catch {
                     console.log("Unable to save the thrusted user to a file, this will not be persistent")
                 }
@@ -155,14 +183,14 @@ async function getThrustedUsers() {
                 console.log("What you entered is not a WhatsApp user, please retry")
                 throw new Error()
             }
-        });
+        })
     }
 }
 
 function getBannedUsers() {
-    if (fs.existsSync('./banned_users.json')) {
+    if (fs.existsSync("./banned_users.json")) {
         try {
-            bannedUsers = JSON.parse(fs.readFileSync('./banned_users.json').toString())
+            bannedUsers = JSON.parse(fs.readFileSync("./banned_users.json").toString())
         } catch {
             console.log("Unable to parse previously saved banned users, please back them up and delete the file")
             throw new Error()
@@ -216,9 +244,9 @@ async function addUser(number, type) {
         
         try {
             if (type == "thrusted") {
-                fs.writeFileSync('./thrusted_users.json', JSON.stringify(thrustedUsers, null, '\t'))
+                fs.writeFileSync("./thrusted_users.json", JSON.stringify(thrustedUsers, null, "\t"))
             } else {
-                fs.writeFileSync('./banned_users.json', JSON.stringify(bannedUsers, null, '\t'))
+                fs.writeFileSync("./banned_users.json", JSON.stringify(bannedUsers, null, "\t"))
             }
         } catch {
             console.log("Unable to save the new user to a file, this will not be persistent")
@@ -234,18 +262,37 @@ async function banUsers() {
     return true
 }
 
-function resetLinks() {
-    var successfull = []
-    var failed = []
-    myGroups.forEach(async element => {
-        try {
-            var newLink = await conn.revokeInvite(element)
-            successfull.push(newLink)
-            successfull.push(element)
-        } catch {
-            failed.push(element)
-        }
-    })
+async function resetLinks() {
+    var myGroupsLinksTmp = []
+
+    for (var i = 0; i < myGroups.length; i++) {
+        setTimeout(async () => {
+            try {
+                myGroupsLinksTmp.push(await conn.revokeInvite(myGroups[i].jid))
+            } catch {
+                myGroupsLinksTmp.push(myGroupsLinks[i])
+            }
+        }, getRandomInt(1000))
+    }
+
+    myGroupsLinks = myGroupsLinksTmp
+
+    return printGroups()
+}
+
+function printGroups() {
+    var list = ""
+
+    for (var i = 0; i < myGroups.length; i++) {
+        list += myGroups[i].name + "\nhttps://chat.whatsapp.com/" + myGroupsLinks[i] + "\n\n"
+    }
+
+    try {
+        fs.writeFileSync("./groups.txt", list)
+        return list
+    } catch {
+        return "I couldn't save what I did to a file, i'll try to append it here\n" + list
+    }
 }
 
 function getRandomInt(max) {
